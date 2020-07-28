5c00772e89058d3827603ea282be2096cbd5b938
Zachary Silver <zmansilver@gmail.com>
2020-07-27 17:03:00 -0700

    added colors for dwm status2d patch

--- ui.c
+++ ui.c
@@ -21,45 +21,49 @@ static const char *const BatteryIcon25 = "";
 static const char *const BatteryIcon0 = "";
 static const char *const TimeFormat = "%l:%M %p";
 static const char *const DateFormat = "%A, %B %_d %Y";
+static const char *const BlueFG = "^c#4e8cb7^";
+static const char *const LightBlueFG = "^c#68a7d4^";
+static const char *const WhiteFG = "^c#ffffff^";
+static const char *const BlackFG = "^c#4d5261^";
+static const char *const PinkFG = "^c#f7a8b8^";
+static const char *const OrangeFG = "^c#eb8e54^";
+static const char *const SchemeReset = "^d^";
 static const float Gigabyte = 1024 * 1024 * 1024;
 
 void setOutput(dwmStatus *status)
 {
-    int volume, usedMemory, totalMemory, usedDisk, totalDisk, outputLength;
+    int volume, usedMemory, totalMemory, outputLength;
 
     volume = status->volume.percent + 0.5;
     usedMemory = status->memory.usedBytes / Gigabyte + 0.5;
     totalMemory = status->memory.totalBytes / Gigabyte + 0.5;
-    usedDisk = status->disk.usedBytes / Gigabyte + 0.5;
-    totalDisk = status->disk.totalBytes / Gigabyte + 0.5;
 
     if (status->wifi.active) {
         outputLength = strlen(status->output);
         snprintf(status->output + outputLength,
                  MAX_STATUS_OUTPUT - outputLength,
-                 "%s %.1f%%  ", status->wifi.icon, status->wifi.strength);
+                 "%s%s%s %.1f%%  ", BlueFG, status->wifi.icon, SchemeReset, status->wifi.strength);
     }
 
     outputLength = strlen(status->output);
     snprintf(status->output + outputLength, MAX_STATUS_OUTPUT - outputLength,
-             "%s %.1f%%  %s %d/%dG  %s %d/%dG  %s %d%%  ",
-             status->cpu.icon, status->cpu.utilization,
-             status->memory.icon, usedMemory, totalMemory,
-             status->disk.icon, usedDisk, totalDisk,
-             status->volume.icon, volume);
+             "%s%s%s %.1f%%  %s%s%s %d/%dG  %s%s%s %d%%  ",
+             BlueFG, status->cpu.icon, SchemeReset, status->cpu.utilization,
+             BlueFG, status->memory.icon, SchemeReset, usedMemory, totalMemory,
+             BlueFG, status->volume.icon, SchemeReset, volume);
 
     if (status->battery.active) {
         outputLength = strlen(status->output);
         snprintf(status->output + outputLength,
-                 MAX_STATUS_OUTPUT - outputLength, "%s %.0f%%  ",
-                 status->battery.icon, status->battery.percent);
+                 MAX_STATUS_OUTPUT - outputLength, "%s%s%s %.0f%%  ",
+                 BlueFG, status->battery.icon, SchemeReset, status->battery.percent);
     }
 
     outputLength = strlen(status->output);
     snprintf(status->output + outputLength, MAX_STATUS_OUTPUT - outputLength,
-             "%s %s  %s%s",
-             status->date.icon, status->date.output,
-             status->time.icon, status->time.output);
+             "%s%s%s %s  %s%s%s%s",
+             BlueFG, status->date.icon, SchemeReset, status->date.output,
+             BlueFG, status->time.icon, SchemeReset, status->time.output);
 }
 
 void setDateFormat(dwmDate *date)
